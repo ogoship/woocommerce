d128e7292b437e62c05dd2531515e21f795efde2
Ilkka Loppi <loppi@iki.fi>
2017-11-07 12:23:58 +0200

    Fix bug with getting latest changes

--- woocommerce-nettivarasto-api/lib/API.php
+++ woocommerce-nettivarasto-api/lib/API.php
@@ -64,10 +64,11 @@ class NettivarastoAPI
     $now = new DateTime('NOW');
     if($now->getTimestamp() - 604800 > $this->timestamp){ // 7*24*60*60
       $restClient = new NettivarastoAPI_RESTclient($this, 'GET', '/LatestChanges', array('order','latestchanges', $now->getTimestamp() - 604800));
+      $restClient->addGetParameter('TimeStamp', $now->getTimestamp() - 604800);
     } else {
       $restClient = new NettivarastoAPI_RESTclient($this, 'GET', '/LatestChanges', array('order','latestchanges', $this->timestamp));
+      $restClient->addGetParameter('TimeStamp', $this->timestamp);
     }
-    $restClient->addGetParameter('TimeStamp', $this->timestamp);
     $resultArray = array();
     $success = $restClient->execute($resultArray);
     
